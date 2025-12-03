b4d6e52aa2f56d3216071ea72c438023043c7529
juanky2332-oss <juanky2332@gmail.com>
2025-12-03 16:23:47 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, (process as any).cwd(), '');
-  
-  return {
-    plugins: [react()],
-    server: {
-      host: true
-    },
-    build: {
-      outDir: 'dist',
-      sourcemap: false
-    },
-    define: {
-      // Polyfill process.env.API_KEY so the SDK works in the browser
-      'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY)
-    }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 })
