bb1704ec1b08f19d5089b2a91a29bd19130ba7e5
juanky2332-oss <juanky2332@gmail.com>
2025-12-03 16:22:25 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { UploadArea } from './components/UploadArea';
 import { ResultCard } from './components/ResultCard';
 import { BatchSummary } from './components/BatchSummary';
